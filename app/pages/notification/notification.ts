c0b6d44b28c2850a9796a364720a8585fbf4d44d
jagruti <jagruti@wohlig.com>
2016-08-30 17:58:58 +0530

    notification

---
+++ app/pages/notification/notification.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/notification/notification.html'
+})
+export class HomePage {
+  constructor(private navCtrl: NavController) {
+
+  }
+}
